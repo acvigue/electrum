f0f66b54696b99999c1738f8b063d7a031f48657
Aiden Vigue <aiden@vigue.me>
2025-11-11 01:09:28 -0500

    fix healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [ "${TESTNET,,}" = "true" ] || [ "${TESTNET}" = "1" ]; then
-    electrum --testnet getinfo > /dev/null 2>&1
-else
-    electrum getinfo > /dev/null 2>&1
+
+CMDARGS=""
+if [ -n "${ELECTRUM_RPC_USERNAME}" ]; then
+    CMDARGS="${CMDARGS} --rpcuser ${ELECTRUM_RPC_USERNAME}"
 fi
+
+if [ -n "${ELECTRUM_RPC_PASSWORD}" ]; then
+    CMDARGS="${CMDARGS} --rpcpassword ${ELECTRUM_RPC_PASSWORD}"
+fi
+
+if [ -n "${TESTNET}" ] && { [ "${TESTNET,,}" = "true" ] || [ "${TESTNET}" = "1" ]; }; then
+    CMDARGS="${CMDARGS} --testnet"
+fi
+
+electrum ${CMDARGS} getinfo > /dev/null 2>&1
